e6dd1d0f446c18a61297e9c359b51de0a3dac5bc
gtonchev <dionigi@abv.bg>
2013-07-31 16:04:53 +0300

    Commit IDbConnection.

--- STS Data Adapter/STSConnection.cs
+++ STS Data Adapter/STSConnection.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using STSdb4.Database;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,64 +13,110 @@ namespace STS.Workbench.STS_Data_Adapter
     {
         #region IDbConnection
 
+        private StorageEngine engine;
+        private ConnectionState state;
+
+        private string dataBase;
+        private string version;
+
+        private string systemFile;
+        private string dataFile;
+
+        public string ConnectionString { get; set; }
+
+        public StorageEngine Engine { get { return engine; } }
+
+        public ConnectionState State { get { return state; } }
+
+        public string Database { get { return dataBase; } }
+
+        public string ServerVersion { get { return version; } }
+
+        public string DataSource { get { return dataFile; } }
+
+        public string SystemSource { get { return systemFile; } }
+
+        public int ConnectionTimeout { get; set; }
+
+        public STSConnection()
+        {
+            state = ConnectionState.Closed;
+        }
+
+        public STSConnection(string connectionString)
+            : this()
+        {
+            ConnectionString = connectionString;
+        }
+       
+        public void ChangeDatabase(string connectionString)
+        {
+            Close();
+
+            ConnectionString = connectionString;
+
+            Open();
+        }
+
+        public void Close()
+        {
+            engine.Close();
+            state = ConnectionState.Closed;
+        }
+
+        public IDbCommand CreateCommand()
+        {
+            return new STSCommand();
+        }
+
+        public void Open()
+        {
+            if (IsValidConnectionString())
+                engine = (StorageEngine)STSdb4.Database.STSdb.FromFile(systemFile, dataFile); // edited
+            else
+                throw new Exception("Connection failed.");
+
+            state = ConnectionState.Open;
+        }
+
+        //@"Provider=STSDb.4.0;System Source=D:\stsdb4.sys;Data Source=D:\data.stsdb4"
+        private bool IsValidConnectionString()
+        {
+            if (ConnectionString == null)
+                return false;
+
+            string[] fields = ConnectionString.Split(';');
+
+            try
+            {
+                dataBase = fields[0].Substring(9, 5);
+                version = fields[0].Substring(15);
+
+                string[] _systemFile = fields[1].Split('\\');
+                systemFile = _systemFile[_systemFile.Length - 1];
+
+                string[] _dataFile = fields[2].Split('\\');
+                dataFile = _dataFile[_dataFile.Length - 1];
+            }
+            catch { return false; }
+
+            if (dataBase != "STSDb" || version != "4.0" || !File.Exists(systemFile) || !File.Exists(dataFile))
+                return false;
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
         public IDbTransaction BeginTransaction(IsolationLevel il)
         {
             throw new NotImplementedException();
         }
 
         public IDbTransaction BeginTransaction()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ChangeDatabase(string databaseName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Close()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string ConnectionString
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public int ConnectionTimeout
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public IDbCommand CreateCommand()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string Database
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public void Open()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ConnectionState State
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public void Dispose()
         {
             throw new NotImplementedException();
         }
