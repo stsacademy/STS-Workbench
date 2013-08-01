6faafc4e0bdadf0add60d2869e1ded55a5e65391
nikolabintev <nikolabintev@abv.bg>
2013-08-01 13:55:37 +0300

    Commit.

--- Program.cs
+++ Program.cs
@@ -16,8 +16,7 @@ namespace STS.Workbench
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            //asdasdasd
+            Application.Run(new MainForm());
         }
     }
 }
