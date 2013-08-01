1e2529f897d8fd360690cdac2791707252c7f16a
gtonchev <dionigi@abv.bg>
2013-08-01 13:46:11 +0300

    Commit.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using STSdb4.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,10 +15,31 @@ namespace STS.Workbench
         [STAThread]
         static void Main()
         {
+            using (IStorageEngine engine = STSdb.FromFile("stsdb4.sys", "stsdb4.dat"))
+            {
+                IIndex<int, string> table = engine.OpenXIndex<int, string>("table");
+
+                for (int i = 0; i < 1000000; i++)
+                {
+                    table[i] = i.ToString();
+                }
+
+                table.Flush();
+                engine.Commit();
+            }
+
+
+            STS_Data_Adapter.STSConnection connection = new STS_Data_Adapter.STSConnection(@"Provider=STSDb.4.0;System Source=D:\stsdb4.sys;Data Source=D:\stsdb4.dat");
+
+            connection.Open();
+            connection.State.ToString();
+
+            connection.Close();
+            connection.State.ToString();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //asdasdasd
         }
     }
 }
