2435d976314b8818278cb9004ea81bf9f8872251
gtonchev <dionigi@abv.bg>
2013-08-02 14:51:58 +0300

    Parameter

--- STS Data Adapter/STSParameter.cs
+++ STS Data Adapter/STSParameter.cs
@@ -11,80 +11,138 @@ namespace STS.Workbench.STS_Data_Adapter
     {
         #region IDataParameter
 
+        DbType m_dbType = DbType.Object;
+        ParameterDirection m_direction = ParameterDirection.Input;
+        bool m_fNullable = false;
+        string m_sParamName;
+        string m_sSourceColumn;
+        DataRowVersion m_sourceVersion = DataRowVersion.Current;
+        object m_value;
+
+        public STSParameter()
+        {
+        }
+
+        public STSParameter(string parameterName, DbType type)
+        {
+            m_sParamName = parameterName;
+            m_dbType = type;
+        }
+
+        public STSParameter(string parameterName, object value)
+        {
+            m_sParamName = parameterName;
+            this.Value = value;
+            // Setting the value also infers the type.
+        }
+
+        public STSParameter(string parameterName, DbType dbType, string sourceColumn)
+        {
+            m_sParamName = parameterName;
+            m_dbType = dbType;
+            m_sSourceColumn = sourceColumn;
+        }
+
         public DbType DbType
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return m_dbType; }
+            set { m_dbType = value; }
         }
 
         public ParameterDirection Direction
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return m_direction; }
+            set { m_direction = value; }
         }
 
         public bool IsNullable
         {
-            get { throw new NotImplementedException(); }
+            get { return m_fNullable; }
+            set { m_fNullable = value; }
         }
 
         public string ParameterName
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return m_sParamName; }
+            set { m_sParamName = value; }
         }
 
         public string SourceColumn
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return m_sSourceColumn; }
+            set { m_sSourceColumn = value; }
         }
 
         public DataRowVersion SourceVersion
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return m_sourceVersion; }
+            set { m_sourceVersion = value; }
         }
 
         public object Value
         {
             get
             {
-                throw new NotImplementedException();
+                return m_value;
             }
             set
             {
-                throw new NotImplementedException();
+                m_value = value;
+                m_dbType = _inferType(value);
+            }
+        }
+
+        private DbType _inferType(Object value)
+        {
+            switch (Type.GetTypeCode(value.GetType()))
+            {
+                case TypeCode.Empty:
+                    throw new SystemException("Invalid data type");
+
+                case TypeCode.Object:
+                    return DbType.Object;
+
+                case TypeCode.DBNull:
+                case TypeCode.Char:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    // Throw a SystemException for unsupported data types.
+                    throw new SystemException("Invalid data type");
+
+                case TypeCode.Boolean:
+                    return DbType.Boolean;
+
+                case TypeCode.Byte:
+                    return DbType.Byte;
+
+                case TypeCode.Int16:
+                    return DbType.Int16;
+
+                case TypeCode.Int32:
+                    return DbType.Int32;
+
+                case TypeCode.Int64:
+                    return DbType.Int64;
+
+                case TypeCode.Single:
+                    return DbType.Single;
+
+                case TypeCode.Double:
+                    return DbType.Double;
+
+                case TypeCode.Decimal:
+                    return DbType.Decimal;
+
+                case TypeCode.DateTime:
+                    return DbType.DateTime;
+
+                case TypeCode.String:
+                    return DbType.String;
+
+                default:
+                    throw new SystemException("Value is of unknown data type");
             }
         }
 
