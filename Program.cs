15f6c5af2cde9c70f7edc7f217b9cff0048baa79
yvalkov <vulkov91@gmail.com>
2013-08-02 10:05:09 +0300

    Commit.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using STSdb4.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,6 +19,20 @@ namespace STS.Workbench
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             //asdasdasd
+
+            List<Schema_Table.Column> list = new List<Schema_Table.Column>();
+             
+            list.Add(new Schema_Table.Column("FirstName", DataType.String, false));
+            list.Add(new Schema_Table.Column("MiddleName", DataType.String, false));
+            list.Add(new Schema_Table.Column("FAQ", DataType.Int32, true));
+            list.Add(new Schema_Table.Column("LastName", DataType.String, false));
+            list.Add(new Schema_Table.Column("ID", DataType.Int64, true));
+
+            Schema_Table.SchemaTable schTable = new Schema_Table.SchemaTable("mydatabase", true, "D:\\TestSchema");
+
+            schTable.CreateTable("Table", list.ToArray());
+
+            schTable.AddEntry("Table", new object[] { "a", "b", "c" });
         }
     }
 }
